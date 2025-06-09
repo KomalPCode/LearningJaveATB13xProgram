ca64d629e047eb375581a2fa48d629c8474fe5cb
KomalPCode <komalgithub1507@gmail.com>
2025-06-09 20:00:35 +0530

    Lab002_HW_Extra updated

--- src/ex_01_Java_Basic/Lab002_HW_Extra.java
+++ src/ex_01_Java_Basic/Lab002_HW_Extra.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic;
 public class Lab002_HW_Extra {
     public static void main(String[] args) {
         System.out.println(4+4);
+        System.out.println(100+100);
     }
 }
