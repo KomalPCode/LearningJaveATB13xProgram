c0acf063ebc7a8caba652bfd918b7a57b4df5772
KomalPCode <komalgithub1507@gmail.com>
2025-06-10 17:36:57 +0530

    9th June programs

---
+++ src/ex_01_Java_Basic/Lab008_Comments.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab008_Comments {
+//  Comment Program
+
+    /*Author - Komal
+    Topic - Automation Testing
+    This is a documentation Comment
+    @param args command line arguments
+    @return void*/
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/ex_01_Java_Basic/My_Program.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class My_Program {
+    public static void main(String[] Thiscanbeanything) {
+        System.out.println("Checkings args");
+    }
+}
