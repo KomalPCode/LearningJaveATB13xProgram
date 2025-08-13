708c68cabfe9aac13fc1ffea62f53a49b09ee034
KomalPCode <komalgithub1507@gmail.com>
2025-08-13 16:37:28 +0530

    30th June Programs

---
+++ src/ex_17_OOPs/Lab166_OOPs.java
@@ -0,0 +1,8 @@
+package ex_17_OOPs;
+
+public class Lab166_OOPs {
+    public static void main(String[] args) {
+
+        Person amit = new Person();
+    }
+}
---
+++ src/ex_17_OOPs/Person.java
@@ -0,0 +1,28 @@
+package ex_17_OOPs;
+
+public class Person {
+
+    String name;
+    int age;
+    String height;
+    String gender;
+    boolean is_married;
+
+    void walk() {
+        System.out.println("walking");
+    }
+
+    void eat(String food) {
+        System.out.println("eating");
+    }
+
+    String sleep() {
+        System.out.println("sleeping");
+        return null:
+    }
+
+    String food(int a) {
+        System.out.println("foody");
+        return null;
+    }
+}
