254817bded1292d1670480e2545af0a7cc2f3a43
KomalPCode <komalgithub1507@gmail.com>
2025-10-31 17:26:53 +0530

    10th June Task Program updated

--- src/Tasks/Task_10_June.java
+++ src/Tasks/Task_10_June.java
@@ -1,6 +1,6 @@
 package Tasks;
 
-public class Task_10_June {
+/*public class Task_10_June {
 //    Print table of 9 using printf
     public static void main(String[] args) {
         int a_0 = 9;
@@ -17,5 +17,22 @@ public class Task_10_June {
         System.out.printf("%d x %d = %d"+"\n", a_0, a_1+8, a_0*(a_1+8));
         System.out.printf("%d x %d = %d"+"\n", a_0, a_1+9, a_0*(a_1+9));
 
+    }
+}*/
+
+public class Task_10_June{
+    public static void main(String[] args){
+
+        int a = 9;
+        System.out.printf("%d x 1 = %d\n",a,a*1);
+        System.out.printf("%d x 2 = %d\n",a,a*2);
+        System.out.printf("%d x 3 = %d\n",a,a*3);
+        System.out.printf("%d x 4 = %d\n",a,a*4);
+        System.out.printf("%d x 5 = %d\n",a,a*5);
+        System.out.printf("%d x 6 = %d\n",a,a*6);
+        System.out.printf("%d x 7 = %d\n",a,a*7);
+        System.out.printf("%d x 8 = %d\n",a,a*8);
+        System.out.printf("%d x 9 = %d\n",a,a*9);
+        System.out.printf("%d x 10 = %d\n",a,a*10);
     }
 }
